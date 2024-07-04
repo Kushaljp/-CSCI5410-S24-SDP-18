6dd5df135f523a2d582fa8dfc8a0dfcf3a44b428
Roshni Joshi <rs888392@dal.ca>
2024-07-04 15:28:44 -0300

    Added routes for register and login

--- frontend/dal-vacation-home/src/App.js
+++ frontend/dal-vacation-home/src/App.js
@@ -6,7 +6,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
