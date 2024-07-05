fbcbac8b055d860eec0312b3c8dc4e0a222f3749
Roshni Joshi <rs888392@dal.ca>
2024-07-04 21:12:00 -0300

    Added second and third factor lambda functions

---
+++ function/UserAuthentication/second-factor-auth/src/main/java/second/factor/auth/SecondFactorAuthLambdaRequestHandler.java
@@ -0,0 +1,27 @@
+package second.factor.auth;
+
+import java.util.Map;
+
+import org.json.JSONObject;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class SecondFactorAuthLambdaRequestHandler implements RequestHandler<Map<String, String>, Boolean> {
+
+	@Override
+	public Boolean handleRequest(Map<String, String> input, Context context) {
+		String inputSecurityQA = input.get("data");
+		String email = input.get("email");
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate
+				.exchange("https://kiuy4j7k8h.execute-api.us-east-1.amazonaws.com/prod/users/" + email, HttpMethod.GET, null, String.class);
+		JSONObject jsonObject = new JSONObject(response.getBody());
+		String userSecurityQA = ((JSONObject) jsonObject.getJSONArray("Items").get(0)).getJSONObject("securityQuestions").get("S").toString();
+		return inputSecurityQA.equals(userSecurityQA);
+	}
+
+}
