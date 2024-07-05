af1e933f050fc556e1ec5622a16aa505c4293f1c
patel7 <pr410642@dal.ca>
2024-07-05 18:36:17 -0300

    Commenting in progress modules for now.

--- frontend/dal-vacation-home/src/App.js
+++ frontend/dal-vacation-home/src/App.js
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './components/user-authentication/Login';
 import Register from './components/user-authentication/Register';
-import  AddProperty  from './components/AddProperty';
-import  RoomDetails  from './components/RoomDetails';
-import ListApprovals from './components/ListApprovals';
-import ListRooms from './components/ListRooms';
+// import  AddProperty  from './components/AddProperty';
+// import  RoomDetails  from './components/RoomDetails';
+// import ListApprovals from './components/ListApprovals';
+// import ListRooms from './components/ListRooms';
 import AgentDashboard from './components/dashboard/AgentDashboard';
 import UserDashboard from './components/dashboard/UserDashboard'
 
@@ -16,10 +16,10 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/agentdashboard" element={<AgentDashboard/>} />
         <Route path="/userdashboard" element={<UserDashboard/>} />
-        <Route path="/addproperty" element={<AddProperty/>} />
+        {/* <Route path="/addproperty" element={<AddProperty/>} />
         <Route path="/roomdetails" element={<RoomDetails/>} />
         <Route path="/listrooms" element={<ListRooms/>} />
-        <Route path="/listapprovals" element={<ListApprovals/>} /> 
+        <Route path="/listapprovals" element={<ListApprovals/>} />  */}
       </Routes>
     </BrowserRouter>
   );
