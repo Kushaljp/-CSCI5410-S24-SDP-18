f602f08bedc7c20791caa77fb8342475ee3f17a4
patel7 <pr410642@dal.ca>
2024-07-05 17:50:23 -0300

    Adding embedded dashboard link.

---
+++ frontend/dal-vacation-home/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const Dashboard = () => {
+    const embedUrl = "https://lookerstudio.google.com/embed/u/0/reporting/4939d1fa-0ba3-4d2f-835d-6741275d7d66/page/lkM4D";
+  return (
+    <Box sx={{ width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column' }}>
+    <Box sx={{ flex: 1}}>
+      <iframe
+        src={embedUrl}
+        style={{ border: 0, width: '100%', height: '100%' }}
+        allowFullScreen
+        title="Dal Vacation - Home Dashboard"
+      ></iframe>
+    </Box>
+  </Box>
+  );
+};
+
+export default Dashboard;
