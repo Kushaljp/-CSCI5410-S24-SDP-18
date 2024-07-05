b34f8d7d15feba6d5ca1ca74875748400f6b78d2
patel7 <pr410642@dal.ca>
2024-07-05 17:53:32 -0300

    Added dashboard page to app.js

--- frontend/dal-vacation-home/src/App.js
+++ frontend/dal-vacation-home/src/App.js
@@ -1,12 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './components/user-authentication/Login';
 import Register from './components/user-authentication/Register';
+import  AddProperty  from './components/AddProperty';
+import  RoomDetails  from './components/RoomDetails';
+import ListApprovals from './components/ListApprovals';
+import ListRooms from './components/ListRooms';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        {/* <Route path="/addproperty" element={<AddProperty/>} />
+        <Route path="/roomdetails" element={<RoomDetails/>} />
+        <Route path="/listrooms" element={<ListRooms/>} />
+        <Route path="/listapprovals" element={<ListApprovals/>} />  */}
+        <Route path="/dashboard" element={<Dashboard/>} />
       </Routes>
     </BrowserRouter>
   );
