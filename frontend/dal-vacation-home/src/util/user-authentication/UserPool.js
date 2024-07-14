973dda73f9e4e7f9978c1e8677df036244baf000
Roshni Joshi <rs888392@dal.ca>
2024-07-14 00:25:54 -0300

    Added cloudformation and added changes

--- frontend/dal-vacation-home/src/util/user-authentication/UserPool.js
+++ frontend/dal-vacation-home/src/util/user-authentication/UserPool.js
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from 'amazon-cognito-identity-js';
 
 const poolData = {
-  UserPoolId: 'us-east-1_PLBXmIkIS',
-  ClientId: '56qh5va2dh3qcj40clp5io0jhm',
+  UserPoolId: 'us-east-1_wy07lyZvM',
+  ClientId: '4iaev7ktoiq4ho7rg0gliomrhb',
 };
 
 export default new CognitoUserPool(poolData);
